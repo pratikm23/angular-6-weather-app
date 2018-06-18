b0fce4d9d9245f763f26fd91b57e7a1b49b702e6
Pratik Mehta <pratikm23@gmail.com>
2018-06-18 15:54:12 +0530

    minor changes

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 var config = {
     url: "http://api.openweathermap.org/data/2.5/forecast",
-    APPID: "ADD YOUR OPEN WEATHER API KEY"
+    APPID: "ADD YOUR OPEN WEATHER API KEY "
 }
 
 export default config;
